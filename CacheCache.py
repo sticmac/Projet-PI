22a1e4add20605c5c42e3ebfef4f595d64cddcff
sticmac <jujulem06@hotmail.fr>
2014-12-17 22:34:53 +0100

    Update CacheCache.py

--- CacheCache.py
+++ CacheCache.py
@@ -9,10 +9,13 @@ coordonneesTortue = [0,0]
 
 #Gestion des événements
 def deplacements():
+    #Applique les fonctions événementielles aux touches directionnelles
     onkey(haut, "Up")
     onkey(bas, "Down")
     onkey(droite, "Right")
     onkey(gauche, "Left")
+    #Touche de selection de case
+    onkey(selectionner, "space")
 
 def haut():
     coordonneesTortue[1]-=1
@@ -44,11 +47,35 @@ def gauche():
         setCoordonnees(coordonneesTortue)
     else:
         coordonneesTortue[0]+=1
+
+def selectionner():
+    #On fixe les valeurs de la case où la fonction d'applique (pour éviter les bugs)
+    x = coordonneesTortue[0]
+    y = coordonneesTortue[1]
+
+    #Ecrit le message correspondant à la valeur de la case
+    #0 = Plouf, 1 = Virus, 2 = Antidote, 3 = Bonus
+    objet = objetsCases[x][y]
+    if objet == 0:
+        write("Plouf !", False, align="center")
+    elif objet == 1:
+        write("Virus", False, align="center")
+    elif objet == 2:
+        write("Antidote", False, align="center")
+    elif objet == 3:
+        write("Bonus", False, align="center")
+    objetsCases[x][y] = -1
+
+    print("Il reste",vie,"habitants en vie")
     
 
 dessinerFenetre(800,600)
-deplacements()
+objetsCases = remplirCases()
+
+vie = 7000000000
+
 
 #Lancement de la boucle principale : passage en programmation événementielle (tout ne peut-être changé que par pression de touche par exemple).
+deplacements()
 listen()
 mainloop()
